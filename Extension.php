187b000675d624340e04c125c5670967ae7842e2
Sam Poyigi <sam@sampoyigi.com>
2020-02-06 00:03:56 +0000

    Minor improvements

--- Extension.php
+++ Extension.php
@@ -8,22 +8,6 @@ use System\Classes\BaseExtension;
 class Extension extends BaseExtension
 {
     /**
-     * Returns information about this extension.
-     *
-     * @return array
-     */
-    public function extensionMeta()
-    {
-        return [
-            'name' => 'Drivers',
-            'author' => 'Igniter',
-            'description' => 'Drivers.',
-            'icon' => 'fa-bug',
-            'version' => '1.0.0',
-        ];
-    }
-
-    /**
      * Register method, called when the extension is first registered.
      *
      * @return void
